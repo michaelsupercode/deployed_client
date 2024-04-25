ef92d57783a9d3660093dcec1c572003c718a44c
michaelsupercode <96441437+michaelsupercode@users.noreply.github.com>
2024-04-25 12:47:42 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,1 @@
-export const apiBaseUrl = process.env.DB_URL || "https://deployed-server-tkfh.onrender.com/"
+export const apiUrl = process.env.API_URL || "https://deployed-server-tkfh.onrender.com/"
